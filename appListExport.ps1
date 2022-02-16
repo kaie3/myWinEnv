911285b35d27a8e0238e718812da5986e4babbde
kaie3 <8e6sh59aes@gmail.com>
2022-02-16 15:20:59 +0900

    更新

--- appListExport.ps1
+++ appListExport.ps1
@@ -1,1 +1,3 @@
-winget export C:\Users\kaie\repository\myWinEnv\mainPcAppList.json | Out-File -Encoding UTF8 C:\Users\kaie\repository\myWinEnv\warning.log
+pwsh -Command {[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8');
+winget export C:\Users\kaie\repository\myWinEnv\mainPcAppList.json > C:\Users\kaie\repository\myWinEnv\warning.log;
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('us-ascii')}
