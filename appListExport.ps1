273e8f56fad2932c1563e23750645ea33dd7791f
kaie3 <8e6sh59aes@gmail.com>
2022-09-24 11:12:33 +0900

    更新

--- appListExport.ps1
+++ appListExport.ps1
@@ -1,3 +1,4 @@
-pwsh -Command {[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8');
+# pwsh -Command {[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8');
+chcp 65001 # 文字コードを今回限り変更。Shif-JIS(932)からutf8(65001)
 winget export C:\Users\kaie\repository\myWinEnv\mainPcAppList.json > C:\Users\kaie\repository\myWinEnv\warning.log;
-[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('us-ascii')}
+# [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('us-ascii')}
